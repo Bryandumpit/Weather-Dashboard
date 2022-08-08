a77c4fa529021ae6df706fc123865d62e6b46e14
Bryan Dumpit <dumpitbr@gmail.com>
2022-08-08 16:10:33 -0400

    polish text fonts and element alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,8 @@ var cityInputEl = document.querySelector("#city");
 
 var weatherReportContainerEl = document.querySelector("#weather-container");
 
+var forecastHeaderContainerEl = document.querySelector("#forecast-header");
+
 var forecastReportContainerEl = document.querySelector("#forecast-container");
 
 var weatherRecallContainerEl = document.querySelector("#recall");
@@ -133,10 +135,11 @@ var displayCityWeatherForecast = function(cityWeather) {
     console.log(weatherIconEl);
 
     var cityContainerEl = document.createElement("div");
-    var cityNameEl = document.createElement("p")
+    cityContainerEl.setAttribute("class","d-flex flex-row flex-wrap align-items-center justify-content-around")
+    var cityNameEl = document.createElement("h2")
     var city = cityWeather[0]
     var date = moment().format("MM/DD/YY");
-    cityNameEl.textContent= city + ' ' + date + ' ';
+    cityNameEl.textContent= city + ' ' + date;
     cityContainerEl.append(cityNameEl, weatherIconEl);
     console.log(cityNameEl);
     console.log(cityContainerEl);
@@ -186,8 +189,13 @@ var displayCityWeatherForecast = function(cityWeather) {
 
 //----------------------------Forecast Data Handling---------------------------------
 
-    //select out forecast data from cityWeatherForecast array
+    // //create forecast header
 
+    var forecastHeader = document.createElement("h2");
+    forecastHeader.textContent = "5-Day Forecast:"
+    forecastHeaderContainerEl.appendChild(forecastHeader);
+
+    //select out forecast data from cityWeatherForecast array
     console.log(cityWeatherForecast[1]);
     console.log(cityWeatherForecast[1].daily.slice(0,5));
 
@@ -201,10 +209,12 @@ var displayCityWeatherForecast = function(cityWeather) {
         
         var forecastDateIconEl = document.createElement("div")
         var forecastDate = moment().add(i,"day").format("MM/DD/YY");
+        var forecastDateEl = document.createElement("h3")
+        forecastDateEl.textContent=forecastDate;
         var forecastIcon = fiveDayForecast[i].weather[0].icon
         var forecastIconEl = document.createElement("img")
         forecastIconEl.src = 'http://openweathermap.org/img/wn/' + forecastIcon + '@2x.png'
-        forecastDateIconEl.append(forecastDate, forecastIconEl);
+        forecastDateIconEl.append(forecastDateEl, forecastIconEl);
 
         var forecastTemp = fiveDayForecast[i].temp.max;
         var forecastWind = fiveDayForecast[i].wind_speed;
@@ -215,16 +225,18 @@ var displayCityWeatherForecast = function(cityWeather) {
         console.log(forecastWind);
         console.log(forecastHumid);
         //create daily forecast elements
-        var forecastDateEl = document.createElement("p");
-        forecastDateEl.textContent = forecastDate
+        
 
         var forecastTempEl = document.createElement("p");
+        forecastTempEl.setAttribute("class", "text-start")
         forecastTempEl.textContent = 'Temperature: ' + forecastTemp + '\u00B0C';
 
         var forecastWindEl = document.createElement("p");
+        forecastWindEl.setAttribute("class", "text-start")
         forecastWindEl.textContent= 'Wind Speed: ' + forecastWind + 'm/s';
 
         var forecastHumidEl = document.createElement("p");
+        forecastHumidEl.setAttribute("class", "text-start")
         forecastHumidEl.textContent = 'Humidity: ' + forecastHumid + '%';
         //append elements to daily forecaset container; allows for each daily forecast to be treated as separate columns
         forecastContainerEl.append(forecastDateIconEl, forecastTempEl, forecastWindEl, forecastHumidEl);
